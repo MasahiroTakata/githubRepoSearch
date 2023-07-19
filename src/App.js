78076067f92699579bc165911094928fb58c43be
Masahiro Takata <takata005581016@gmail.com>
2023-07-19 22:15:06 +0900

    コメントした

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import {SearchForm} from './components/SearchForm';
+import {SearchForm} from './components/SearchForm'; // 2,3行とも独自に作成したので{}が必要
 import {RepositoryList} from './components/RepositoryList';
 
 export const App = () => {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState([]); //更新関数
 
   const handleSearch = (data) => {
+    console.log(data); // 検索結果が格納されている
     setRepositories(data);
   };
 
   return (
     <div>
       <h1>GitHub Repository Search</h1>
+      {/* onSearchという変数にhandleSearch（中身はdata=>{setRepositories(data);}）を渡している */}
       <SearchForm onSearch={handleSearch} />
+      {/* SearchFormコンポーネントから、repositoriesのデータをもらう */}
       <RepositoryList repositories={repositories} />
     </div>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import styled from "styled-components";
 
 export const SearchForm = ({ onSearch }) => {
+  console.log("データの中身は、" + onSearch); // handleSearch（中身はdata=>{setRepositories(data);})を受け取っている、さらに文字を入力する度に呼ばれている
   const [query, setQuery] = useState(''); // 更新関数
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // 画面リロードを制御する処理
     try {
       const response = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
       onSearch(response.data.items);
